387128173b94c18b853fae2aa9dbd03a6d2692ef
YuiSanae2f <mincraft20046@gmail.com>
2024-11-12 03:34:22 +0900

    d

--- a.c
+++ a.c
@@ -1,36 +1,72 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <sys/types.h> 
+#include <unistd.h>
+#include <wait.h>
 #include <time.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-FILE* file;
+/* Spawn a child process running a new program. PROGRAM is the name of the program to run; the path will be searched for this program. ARG_LIST is a NULL-terminated list of character strings to be passed as the program's argument list. Returns the process ID of the spawned process. */
+int spawn(const char* program, char** const arg_list)
+{ 
+    pid_t child_pid;
 
-#define __printf(...) fprintf(file, __VA_ARGS__)
-#define __test(n) time = loop(n); __printf("Loop: %d, elapsed: %d, e: %d\n", n, time, time / n);
+    /* Duplicate this process. */ 
+    child_pid = fork();
+    if (child_pid != 0)
+    /* This is the parent process. */
+    return child_pid;
 
-clock_t loop(int a) {
-    clock_t start = clock(), end;
+    else {
+        /* Now execute PROGRAM, searching for it in the path. */
+        execvp(program, arg_list);
 
-    for(int i = 0; i < a; i++) {
-        system("ls -l /");
+        /* The execvp function returns only if an error occurs. */
+        perror("an error occurred in execvp\n");
+        abort();
     }
-
-    end = clock();
-
-    return end - start;
 }
 
-int main() {
-    file = fopen("Test.txt", "w");
-    clock_t start, end;
+int main()
+{
+    // To separate the time measured code (for convenient)
+    FILE* _ = fopen("_a", "w");
+
+    clock_t 
+        global_time = clock(),
+        global_time_end,
+        loc_time = clock(),
+        loc_time_end,
+        loc_time_sum = 0;
+
+    /* The argument list to pass to the "Is" command. */ 
+    char* arg_list[] = {
+        "ls", /* argv[0], the name of the program. */
+        "-l",
+        "/",
+        NULL /* The argument list must end with a NULL. */
+    };
+
+    for(int i = 0; i < 5; i++) {
+        loc_time = clock();
+        /* Spawn a child process running the "Is" command. Ignore the
+        returned child process ID. */ 
+        spawn("ls", arg_list);
+
+        wait(0); // wait until child process die
+
+        loc_time_end = clock();
+        printf("loc time %d, %d\n", i, loc_time_end - loc_time);
+        fprintf(_, "loc time %d, %d\n", i, loc_time_end - loc_time);
+        loc_time_sum += loc_time_end - loc_time;
+    }
+
+    global_time_end = clock();
+    printf("done with main program, elapsed global: %d\n", global_time_end - global_time);
+    fprintf(_, "done with main program, elapsed global: %d\n", global_time_end - global_time);
+
+    printf("local time sum: %d\n", loc_time_sum);
+    fprintf(_, "local time sum: %d\n", loc_time_sum);
     
-    int time;
-
-    __test(1);
-    __test(100);
-    __test(1000);
-    __test(10000);
-    __test(100000);
-
-    fclose(file);
+    fclose(_); // close it
     return 0;
 }
